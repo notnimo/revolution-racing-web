f6eaa1f1e23e8a1fbfc065443d6ba71c502f0a4c
not_nimo <damiano.livoni@gmail.com>
2026-01-25 17:32:56 +0000

    global styles added, styled and added contacts

---
+++ src/ui/home/contacts.tsx
@@ -0,0 +1,8 @@
+export function Contacts() {
+	return (
+		<div className="w-full">
+			<h1>Contacts</h1>
+			<p>Get in touch with the Revolution Racing team.</p>
+		</div>
+	);
+}
--- src/ui/home/hero-section.tsx
+++ src/ui/home/hero-section.tsx
@@ -1,6 +1,6 @@
 export function HeroSection() {
 	return (
-		<div className="bg-secondary p-8 h-screen w-full">
+		<div className="bg-rr-black p-8 h-screen w-full">
 			<h1 className="text-4xl font-bold text-white">
 				Welcome to Revolution Racing
 			</h1>
