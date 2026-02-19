d00a577ebd870aabf47192a66781328fa4dd2d4f
not_nimo <damiano.livoni@gmail.com>
2026-02-19 00:00:51 +0000

    moved image files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-/*import { useRef } from "react";
+import { useRef } from "react";
 
 import { NavBar } from "@/src/ui/home/nav-bar/nav-bar";
 import { HeroSection } from "@/src/ui/home/hero-section";
@@ -10,76 +10,50 @@ import { StatsSection } from "@/src/ui/home/stats-section";
 import { Contacts } from "@/src/ui/home/contacts";
 
 export default function Main() {
-	const navbarItems = ["Home", "About", "News", "Stats", "Contact"];
+  const navbarItems = ["Home", "About", "News", "Stats", "Contact"];
 
-	const heroRef = useRef<HTMLDivElement>(null);
-	const aboutRef = useRef<HTMLDivElement>(null);
-	const newsRef = useRef<HTMLDivElement>(null);
-	const statsRef = useRef<HTMLDivElement>(null);
-	const contactRef = useRef<HTMLDivElement>(null);
+  const heroRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const newsRef = useRef<HTMLDivElement>(null);
+  const statsRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
 
-	const sectionRefs: { [key: string]: React.RefObject<HTMLDivElement | null> } =
-		{
-			Home: heroRef,
-			About: aboutRef,
-			News: newsRef,
-			Stats: statsRef,
-			Contact: contactRef,
-		};
+  const sectionRefs: { [key: string]: React.RefObject<HTMLDivElement | null> } =
+    {
+      Home: heroRef,
+      About: aboutRef,
+      News: newsRef,
+      Stats: statsRef,
+      Contact: contactRef,
+    };
 
-	const handleNavClick = (item: string) => {
-		const ref = sectionRefs[item];
-		if (ref?.current) {
-			ref.current.scrollIntoView({ behavior: "smooth" });
-		}
-	};
+  const handleNavClick = (item: string) => {
+    const ref = sectionRefs[item];
+    if (ref?.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-	return (
-		<div className="w-screen flex flex-col justify-start items-center">
-			<div className="sticky top-0 z-50" id="nav-bar-container">
-				<NavBar navbarItems={navbarItems} onItemClick={handleNavClick} />
-			</div>
-			<div ref={heroRef}>
-				<HeroSection />
-			</div>
-			<div ref={aboutRef}>
-				<AboutSnapshot />
-			</div>
-			<div ref={newsRef}>
-				<NewsSnapshot />
-			</div>
-			<div ref={statsRef}>
-				<StatsSection />
-			</div>
-			<div ref={contactRef}>
-				<Contacts />
-			</div>
-		</div>
-	);
-}*/
-
-import { TMCard } from "../ui/about-components/team-mbr-card";
-
-export default function Page() {
   return (
-    <>
-      <div className="flex flex- row h-screen w-screen justify-center items-center">
-        <div className="max-w-[500px] max-h-[500px]">
-          <TMCard
-            teamMember={{
-              name: "Gabriele di Giorgio",
-              position: "Project Manager",
-              description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis possimus nemo ipsa, dicta debitis, nulla labore dolorum recusandae assumenda cumque autem modi quam quod animi? Neque distinctio nisi blanditiis et!",
-              image: {
-                width: 20,
-                height: 20,
-                imageRef: "/foo.jpg",
-              },
-            }}
-          />
-        </div>
+    <div className="w-screen flex flex-col justify-start items-center">
+      <div className="sticky top-0 z-50" id="nav-bar-container">
+        <NavBar navbarItems={navbarItems} onItemClick={handleNavClick} />
       </div>
-    </>
+      <div ref={heroRef}>
+        <HeroSection />
+      </div>
+      <div ref={aboutRef}>
+        <AboutSnapshot />
+      </div>
+      <div ref={newsRef}>
+        <NewsSnapshot />
+      </div>
+      <div ref={statsRef}>
+        <StatsSection />
+      </div>
+      <div ref={contactRef}>
+        <Contacts />
+      </div>
+    </div>
   );
 }
