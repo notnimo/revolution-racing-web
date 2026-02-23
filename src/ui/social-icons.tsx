d6bd9259c767f473d477052bb91890fab475a38b
not_nimo <damiano.livoni@gmail.com>
2026-02-23 23:11:03 +0000

    social logos added

---
+++ src/ui/social-icons.tsx
@@ -0,0 +1,53 @@
+import { Instagram } from "lucide-react";
+import { Linkedin } from "lucide-react";
+import { Youtube } from "lucide-react";
+import { Mail } from "lucide-react";
+import { Facebook } from "lucide-react";
+
+function TikTokLogo() {
+  return (
+    <>
+      <img src="/tiktok.svg" alt="tiktok-logo" />
+    </>
+  );
+}
+
+function InstagramLogo() {
+  return (
+    <>
+      <Instagram />
+    </>
+  );
+}
+
+function FacebookLogo() {
+  return (
+    <>
+      <Facebook />
+    </>
+  );
+}
+
+function LinkedinLogo() {
+  return (
+    <>
+      <Linkedin />
+    </>
+  );
+}
+
+function YoutubeLogo() {
+  return (
+    <>
+      <Youtube />
+    </>
+  );
+}
+
+function MailLogo() {
+  return (
+    <>
+      <Mail />
+    </>
+  );
+}
