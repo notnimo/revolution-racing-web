2fccb5d2eb21151dafa8d31dfc8fd5a211d47e8a
not_nimo <damiano.livoni@gmail.com>
2026-03-02 00:21:13 +0000

    dev: contact continued while waiting for new color palette

--- src/ui/contacts/badge-section.tsx
+++ src/ui/contacts/badge-section.tsx
@@ -1,3 +1,11 @@
-export function BadgeSection() {
-  return <div>Hello World</div>;
+import { type Contact, type ContactPack } from "@/src/lib/contacts";
+
+export function BadgeSection({ contacts }: { contacts: ContactPack }) {
+  const temp = <></>;
+
+  const contactArray: Contact[] = Object.values(contacts) as Contact[];
+  for (const contact of contactArray) {
+    // handle each contact (render badges here)
+  }
+  return temp;
 }
--- src/ui/contacts/contacts.tsx
+++ src/ui/contacts/contacts.tsx
@@ -1,11 +1,12 @@
+import { ContactPack } from "@/src/lib/contacts";
 import { BadgeSection } from "@/src/ui/contacts/badge-section";
 
-export function Contacts() {
+export function Contacts({ contacts }: { contacts: ContactPack }) {
   return (
     <div className="w-screen bg-rr-dark-blue flex flex-col items-center justify-around">
       <hr />
       <h1>Contacts</h1>
-      <BadgeSection />
+      <BadgeSection contacts={contacts} />
     </div>
   );
 }
