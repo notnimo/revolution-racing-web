26e7c96a563c8abdcff4f75e611d4bcfa79a0282
not_nimo <damiano.livoni@gmail.com>
2026-03-17 19:19:32 +0000

    misc: restored stem racing logo; dev: car hero inserted in hero section with gradient background

--- src/ui/home/hero-section.tsx
+++ src/ui/home/hero-section.tsx
@@ -1,11 +1,22 @@
 export function HeroSection() {
-  const containerDivStyle: string = `bg-rr-black p-8 h-screen w-screen`;
   return (
-    <div className={containerDivStyle}>
-      <h1 className="text-4xl font-bold">Welcome to Revolution Racing</h1>
-      <p className="text-lg">
-        Join us in revolutionizing the racing experience.
-      </p>
-    </div>
+    <section className="relative h-screen w-screen overflow-hidden bg-gradient-to-b from-[#3B9FE5] to-white p-8">
+      <div className="absolute inset-0 overflow-hidden">
+        <img
+          src="/car-hero.png"
+          alt="Car Hero"
+          className="hidden md:block h-full w-full object-cover object-center opacity-80"
+        />
+      </div>
+
+      <div className="relative z-10 flex h-full flex-col top-10 items-start justify-start text-white md:text-black">
+        <h1 className="text-4xl font-bold md:text-6xl">
+          Welcome to Revolution Racing
+        </h1>
+        <p className="mt-4 text-lg md:text-xl">
+          Join us in revolutionizing the racing experience.
+        </p>
+      </div>
+    </section>
   );
 }
