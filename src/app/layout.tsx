18bccadbcaf15be123fcd67795b0187e8a494c1c
not_nimo <damiano.livoni@gmail.com>
2026-03-23 00:50:04 +0000

    misc: added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { ReactNode } from "react";
 import "./global.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en" className="example">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en" className="example">
+			<body>{children}</body>
+			<Analytics />
+		</html>
+	);
 }
