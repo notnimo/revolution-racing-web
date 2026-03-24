c99661e9dfeca5a3561dbcf918ad7b3c6a47cad7
not_nimo <damiano.livoni@gmail.com>
2026-03-24 00:35:39 +0000

    dev: reverted badges to light mode and disabled hover effect

--- src/ui/news-components/news-overview/badges.tsx
+++ src/ui/news-components/news-overview/badges.tsx
@@ -1,34 +1,34 @@
 import { Badge } from "@/src/components/badge";
 
 function CompetitionBadge() {
-  return <Badge className="bg-orange-950 text-orange-300">Competition</Badge>;
+	return <Badge className="bg-orange-50 text-orange-700">Competition</Badge>;
 }
 
 function SponsorBadge() {
-  return <Badge className="bg-blue-950 text-blue-300">Sponsor</Badge>;
+	return <Badge className="bg-blue-50 text-blue-700">Sponsor</Badge>;
 }
 
 function EventBadge() {
-  return <Badge className="bg-green-950 text-green-300">Event</Badge>;
+	return <Badge className="bg-green-50 text-green-700">Event</Badge>;
 }
 
 function DevelopmentBadge() {
-  return <Badge className="bg-sky-950 text-sky-300">Development</Badge>;
+	return <Badge className="bg-sky-50 text-sky-700">Development</Badge>;
 }
 
 function OtherBadge() {
-  return <Badge className="bg-gray-950 text-gray-300">Other</Badge>;
+	return <Badge className="bg-gray-50 text-gray-700">Other</Badge>;
 }
 
 function TeamBadge() {
-  return <Badge className="bg-purple-950 text-purple-300">Team</Badge>;
+	return <Badge className="bg-purple-50 text-purple-700">Team</Badge>;
 }
 
 export {
-  CompetitionBadge,
-  SponsorBadge,
-  EventBadge,
-  DevelopmentBadge,
-  OtherBadge,
-  TeamBadge,
+	CompetitionBadge,
+	SponsorBadge,
+	EventBadge,
+	DevelopmentBadge,
+	OtherBadge,
+	TeamBadge,
 };
