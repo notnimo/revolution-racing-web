87ec13a35f060532a627d2036bd1a2d39743b02a
not_nimo <damiano.livoni@gmail.com>
2026-04-25 21:21:26 +0100

    dev: added team presentation

--- src/ui/about-components/team-presentation.tsx
+++ src/ui/about-components/team-presentation.tsx
@@ -1,3 +1,21 @@
 export function TeamPresentation() {
-	return <div>Team Presentation</div>;
+	return (
+		<div className="width-screen flex flex-col md:flex-row items-center gap-10 md:gap-20">
+			<div className="">
+				<img
+					src="/rev-racing-logos/logo-black.png"
+					alt="foo"
+					className="w-[50vmax] rounded-[50%] bg-white"
+				/>
+			</div>
+			<div>
+				<div>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque,
+					magnam! Similique tenetur eaque accusamus omnis facilis cumque
+					pariatur labore reprehenderit recusandae, corrupti excepturi delectus
+					optio tempore iste autem aperiam. Magnam?
+				</div>
+			</div>
+		</div>
+	);
 }
