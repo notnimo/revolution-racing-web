12a829a11edabc2d16fd4a8cc599e51d5cc8f958
not_nimo <damiano.livoni@gmail.com>
2026-05-05 22:23:25 +0100

    dev: added getTM functions

--- src/lib/about/tm.ts
+++ src/lib/about/tm.ts
@@ -1,3 +1,5 @@
+import { default as tmRawData } from "@/src/lib/about/data/tm.json";
+
 import { ImageRef } from "@/src/lib/imageRef";
 import { positionOptions } from "@/src/lib/home/team-members";
 
@@ -8,14 +10,23 @@ type Task = {
 
 type TM = {
 	id: number;
+	name: string;
 	role: positionOptions;
 	portrait: ImageRef<`/team-members/${string}.png`>;
 	description: string;
 	involvement: Task[];
 };
 
-function getTM(): TM[] {
-	return [];
+function getTMs(): TM[] {
+	let tmData: TM[] = [];
+	Object.values(tmRawData).forEach((tm) => tmData.push(tm as TM));
+	return tmData;
+}
+
+function getTM(name: string): TM | undefined {
+	const tms = getTMs();
+	return tms.find((tm) => tm.name === name);
 }
 
 export type { TM, Task };
+export { getTMs, getTM };
